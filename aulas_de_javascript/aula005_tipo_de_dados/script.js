9297eb25b9219031fcccad56e89b14728ad1b96d
Deriksantos-devbr <134239497+Deriksantos-devbr@users.noreply.github.com>
2023-05-31 23:28:22 -0300

    inicio da aula tipo de dados

---
+++ aulas_de_javascript/aula005_tipo_de_dados/script.js
@@ -0,0 +1,15 @@
+var n = 20;
+console.log(typeof(n)); 
+
+var nome = "Derik" 
+console.log(typeof(nome));
+console.log(nome.toLocaleUpperCase());
+
+var nome = 29;
+console.log(typeof(nome));
+
+if(typeof(nome)== "string"){
+    console.log(nome.toLocaleUpperCase());
+}else{
+    console.log("Você precisa usar uma string");
+}
