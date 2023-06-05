5931e77dd8bec8a6dc2c6a1d60aeac47ef6a2eb5
Deriksantos-devbr <134239497+Deriksantos-devbr@users.noreply.github.com>
2023-06-05 20:07:34 -0300

    exercicios javascript

--- aulas_de_javascript/aula006_uso_de_opradores/script.js
+++ aulas_de_javascript/aula006_uso_de_opradores/script.js
@@ -1,31 +1,32 @@
-var n = 3;
-//n = n * 10;
-n *= 10;
-console.log(n);
 
-var x = 10;
-x++
 
-b = 20;
-c = 60;
+var x = 5;
+var a = 3;
+var b = 4;
+var c = 7;
 
- console.log(x);
-
- console.log(n <= x || b != c);
 
 
 
  //Verificar se a é maior que b e b é divisivel por 2  
- console.log(a > b && b % 2 == 0);
+ console.log(a < b && b % 2 == 0);
 
 
  //Verificar se b é maior igual a ou b dividido por 2 o resultado é 2
+ console.log(a <= b || b / 2 == 2)
 
 
 
  //Verificar se a é maior que b e b maior que igual a 5
+ console.log(a < b && 3 <= b );
 
 
  //Verificar se b é par ou impar usando ternario
+
+resultado = b % 2 == 0 ? "Par" : "Impar";
+console.log(resultado)
+
+
+
 
 
