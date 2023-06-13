69130fd15df7604489154bfcbb4b0963102772b0
Deriksantos-devbr <134239497+Deriksantos-devbr@users.noreply.github.com>
2023-06-12 22:55:02 -0300

    aula sobre instrução do ... wehile

---
+++ aulas_de_javascript/aulas_if/instrucao_do_while.js
@@ -0,0 +1,27 @@
+
+
+
+var carro = [{modelo: 'audi A3', marca: 'Audi', ano: '2023'},{modelo: 'Copass', marca: 'Jeep', ano: '2022'}]
+
+for(let caracteristica in carro)
+   console.log(carro[caracteristica].marca);
+
+
+   for(let c of carro){
+      console.log(c.ano)
+
+   }
+
+
+var c =1; 
+    while (c <= 5){
+        console.log(c);
+        c++;
+    }  
+    
+var t = 1;
+
+do{
+   console.log(t);
+   t++
+}while(t < 1)
