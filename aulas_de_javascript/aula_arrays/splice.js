37d80539996e9ee4c5ec5826f2d4d8b5e97ebda8
Deriksantos-devbr <134239497+Deriksantos-devbr@users.noreply.github.com>
2023-06-17 12:16:26 -0300

    novos exercicios

--- aulas_de_javascript/aula_arrays/splice.js
+++ aulas_de_javascript/aula_arrays/splice.js
@@ -1,4 +1,11 @@
-var arr2 = [1,2,3,4,5,6,7];
+var ar = [-5,10,15,20,-3,89];
+var arr2 = [1,2,3,4,5,6,7]
 
 arr2.splice(1,1);
+
 console.log(arr2);
+
+
+var nomes = ["Maria ","João ","Derik ","Pedro"];
+var novos = nomes.splice(1,2, "Luiz ","Ronaldo");
+console.log(nomes);
