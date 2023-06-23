e31324cdeb55c4750ba5a76bce5f330b4cb14672
Deriksantos-devbr <134239497+Deriksantos-devbr@users.noreply.github.com>
2023-06-22 23:47:30 -0300

    exercicios de class e herança js

---
+++ aulas_de_javascript/class/heranca.js
@@ -0,0 +1,26 @@
+class DispositivoEletronico{
+    constructor(nome){
+        this.nome = nome;
+        this.ligado = false;
+    }
+    ligar(){
+        if(this.ligado){
+            console.log("JA esta ligado")
+            return;
+        }
+        this.ligado = true;
+    }
+}
+
+class Smartphone extends DispositivoEletronico{
+    constructor(nome,cor,modelo){
+        super(nome);
+        this.cor = cor;
+        this.modelo = modelo;
+    }
+}
+
+var s1 = new Smartphone("Sansung", "Preto", "A71");
+console.log(s1);
+s1.ligar();
+s1.ligar();
