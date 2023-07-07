9cbdbcaddef86cf642af964bd93edc9ffefb5b60
Deriksantos-devbr <134239497+Deriksantos-devbr@users.noreply.github.com>
2023-07-06 23:45:36 -0300

    novas aulas react

---
+++ pratica_react/aula03_create-react-app/my-app/src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="App">
+    <h1>
+      <span>Olá Derik</span> 
+     </h1>
+    <strong>Tudo bem?</strong>
+    <article>
+      <h2>Subtitulo</h2>
+      <p>varios textotos, varios textotos, varios textotos, varios textotos, varios textotos, </p>
+    </article>
+
+    </div>
+  );
+}
+
+export default App;
