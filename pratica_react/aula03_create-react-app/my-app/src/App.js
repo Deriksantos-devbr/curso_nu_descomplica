22e2e487329a1d77cb27dbd4880d1a2a961910ff
Deriksantos-devbr <134239497+Deriksantos-devbr@users.noreply.github.com>
2023-07-09 10:01:31 -0300

    novas aulas

--- pratica_react/aula03_create-react-app/my-app/src/App.js
+++ pratica_react/aula03_create-react-app/my-app/src/App.js
@@ -1,15 +1,28 @@
-function App() {
+function MeuBotao(props){
+  return (
+    <button type="button">{props.label} - {props.idade}</button> 
+  )
+}
+
+
+function Pessoa(props) {
+  return(
+    <section>
+    voce é {props.idade >= 18 ? 'de maior' : 'de menor'}
+    </section>
+  )
+  }
+function App(){
+  const labelBotao = 'Entre por aqui';
   return (
     <div className="App">
-    <h1>
-      <span>Olá Derik</span> 
-     </h1>
-    <strong>Tudo bem?</strong>
+    <Titulo/>
+    <Pessoa idade={19} />
     <article>
-      <h2>Subtitulo</h2>
-      <p>varios textotos, varios textotos, varios textotos, varios textotos, varios textotos, </p>
+      <h2>SUbtitulo</h2>
+      <p>sua frase de demonstração sera digitada aqui</p>
     </article>
-
+    <MeuBotao label={'${labelBotao} !!!'} idade={30}/>
     </div>
   );
 }
